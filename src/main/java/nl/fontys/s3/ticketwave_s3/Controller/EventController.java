252f01a3c351334de09d44000924390fee07e61e
Anh Huynh <523530@student.fontys.nl>
2024-10-10 23:36:28 +0200

    Add CORS in place for controllers

--- src/main/java/nl/fontys/s3/ticketwave_s3/Controller/EventController.java
+++ src/main/java/nl/fontys/s3/ticketwave_s3/Controller/EventController.java
@@ -11,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/events")
 public class EventController {
