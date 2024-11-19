5037200b1fa242363b85cbce7c5cb2dfca49c06c
Anh Huynh <523530@student.fontys.nl>
2024-11-20 00:00:02 +0100

    fix test fail

--- src/test/java/nl/fontys/s3/ticketwave_s3/TicketwaveS3ApplicationTests.java
+++ src/test/java/nl/fontys/s3/ticketwave_s3/TicketwaveS3ApplicationTests.java
@@ -2,8 +2,10 @@ package nl.fontys.s3.ticketwave_s3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TicketwaveApplicationTests {
 
     @Test
