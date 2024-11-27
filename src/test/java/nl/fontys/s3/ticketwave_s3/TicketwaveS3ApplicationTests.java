ad48296a30ab0593183ae44df7af8aec9cca90ee
Anh Huynh <523530@student.fontys.nl>
2024-11-27 10:48:56 +0100

    remove contextLoad()

--- src/test/java/nl/fontys/s3/ticketwave_s3/TicketwaveS3ApplicationTests.java
+++ src/test/java/nl/fontys/s3/ticketwave_s3/TicketwaveS3ApplicationTests.java
@@ -1,14 +1,7 @@
 package nl.fontys.s3.ticketwave_s3;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class TicketwaveApplicationTests {
-
-    @Test
-    void contextLoads() {
-        // This test will pass if the application context loads successfully.
-    }
 }
