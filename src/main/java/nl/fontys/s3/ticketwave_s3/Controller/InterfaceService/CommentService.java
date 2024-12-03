da010d7f2e1bd1a7aec78dd486fb781a45b03328
Anh Huynh <523530@student.fontys.nl>
2024-12-03 23:41:06 +0100

    Add missing CommentService file

---
+++ src/main/java/nl/fontys/s3/ticketwave_s3/Controller/InterfaceService/CommentService.java
@@ -0,0 +1,10 @@
+package nl.fontys.s3.ticketwave_s3.Controller.InterfaceService;
+
+import nl.fontys.s3.ticketwave_s3.Controller.DTOS.CommentDTO;
+
+import java.util.List;
+
+public interface CommentService {
+    CommentDTO addComment(CommentDTO commentDTO);
+    List<CommentDTO> getCommentsByEventId(Integer eventId);
+}
