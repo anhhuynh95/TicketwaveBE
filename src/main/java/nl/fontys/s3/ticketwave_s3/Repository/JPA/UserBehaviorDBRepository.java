1d10cfdfca2fee496a5c02f85081beb665add7cb
Anh Huynh <523530@student.fontys.nl>
2025-01-06 14:33:05 +0100

    fix pipeline fail

--- src/main/java/nl/fontys/s3/ticketwave_s3/Repository/JPA/UserBehaviorDBRepository.java
+++ src/main/java/nl/fontys/s3/ticketwave_s3/Repository/JPA/UserBehaviorDBRepository.java
@@ -2,11 +2,9 @@ package nl.fontys.s3.ticketwave_s3.Repository.JPA;
 
 import nl.fontys.s3.ticketwave_s3.Repository.Entity.UserBehaviorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserBehaviorDBRepository extends JpaRepository<UserBehaviorEntity, Integer> {
     Optional<UserBehaviorEntity> findByUserId(Integer userId);}
 
--- src/test/java/nl/fontys/s3/ticketwave_s3/TicketwaveS3ApplicationTests.java
+++ src/test/java/nl/fontys/s3/ticketwave_s3/TicketwaveS3ApplicationTests.java
@@ -1,9 +1,14 @@
 package nl.fontys.s3.ticketwave_s3;
 
+import nl.fontys.s3.ticketwave_s3.Configuration.Security.WebSecurityConfig;
+import nl.fontys.s3.ticketwave_s3.Configuration.WebSocketConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class, WebSecurityConfig.class, WebSocketConfig.class})
 class TicketwaveApplicationTests {
 
     @Test
