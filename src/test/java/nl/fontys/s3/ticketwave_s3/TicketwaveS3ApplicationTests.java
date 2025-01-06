1b2201eb707c1146b8c0e8948b0598fad2b4f61a
Anh Huynh <523530@student.fontys.nl>
2025-01-06 12:52:46 +0100

    fix pipeline fail

--- src/test/java/nl/fontys/s3/ticketwave_s3/TicketwaveS3ApplicationTests.java
+++ src/test/java/nl/fontys/s3/ticketwave_s3/TicketwaveS3ApplicationTests.java
@@ -1,7 +1,11 @@
 package nl.fontys.s3.ticketwave_s3;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TicketwaveApplicationTests {
+
+    @Test
+    void contextLoads() {}
 }
