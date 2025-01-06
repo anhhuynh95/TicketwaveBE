ad32e7b4f417399e8dc4339241390d8277d53a07
Anh Huynh <523530@student.fontys.nl>
2025-01-06 12:46:02 +0100

    fix pipeline fail

--- src/test/java/nl/fontys/s3/ticketwave_s3/Controller/CommentControllerIntegrationTest.java
+++ src/test/java/nl/fontys/s3/ticketwave_s3/Controller/CommentControllerIntegrationTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@TestPropertySource("/application-test.properties")
 class CommentControllerIntegrationTest {
 
     @Autowired
